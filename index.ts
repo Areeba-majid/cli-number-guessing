5b33fda5a4cde888dd20938ed9c8bc5bbb553561
Areeba Riaz <160718011+Areeba-majid@users.noreply.github.com>
2024-04-04 11:13:47 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,14 @@
+
+let studentName = ("areeba");
+let time = ("08 am");
+let day = ("thursday");
+
+console.log("today is my class")
+
+if (console.log
+
+)
+    console.log("")
+else {
+    console.log("today is not your day")
+}
